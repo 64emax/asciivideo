6456f49ea7d833b168f8448650c6babeee252f28
64emax <emax642007@gmail.com>
2023-10-13 16:48:04 +0200

    ok so it turns out ualarm is fake posix made it up

--- asciivideo.c
+++ asciivideo.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#ifndef __USE_MISC
-#define __USE_MISC
-#endif
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <signal.h>
+#include <sys/time.h>
 
 #include "stb_image.h"
 #include "stb_image_resize.h"
@@ -22,26 +20,33 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "no video you stupid\n");
 		exit(69);
 	}
-	
+
 	ma_engine engine;
 	ma_engine_init(NULL, &engine);
 
 	char path[256];
-	
+
 	sprintf(path, "%s/audio.mp3", argv[1]);
 	ma_engine_play_sound(&engine, path, NULL);
 
 	size_t FPS = 60; //TODO: mayhaps don't hardcode
 	size_t DT = (1.f/FPS)*1000000.f;
 
+	timer_t t;
+	timer_create(CLOCK_REALTIME, NULL, &t);
+
+	struct itimerspec h = {
+		.it_interval.tv_nsec = DT,
+		.it_value.tv_nsec = DT,
+	};
+	setitimer(0, (struct itimerval * restrict) &h, NULL);
 	signal(SIGALRM, incframe);
-	ualarm(DT, DT);
 
 	size_t prevcol = 0;
 	int imgw, imgh;
 	struct winsize w;
 	size_t OUTW, OUTH;
-	
+
 	while(1){
 		ioctl(0, TIOCGWINSZ, &w);
 		OUTW = w.ws_col;
@@ -62,7 +67,7 @@ int main(int argc, char **argv) {
 		stbi_uc *img = stbi_load(path, &imgw, &imgh, NULL, 3);
 		stbir_resize_uint8(img, imgw, imgh, 0, rimg, OUTW, OUTH, 0, 3);
 		stbi_image_free(img);
-		
+
 		printf("\033[0;0H");
 		for (size_t i = 0; i < OUTW*OUTH*3; i+=3) {
 			if (prevcol == (rimg[i+2] << 16 | rimg[i+1] << 8 | rimg[i])) {
